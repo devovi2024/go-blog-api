a89eb10ced5547dbbdc8505806176ca3d39f0b34
devovi2024 <arfanhosenovi@gmail.com>
2026-06-29 01:10:18 +0600

    HTTP routes and JSON response

--- handler.go
+++ handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -11,4 +12,13 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 func AboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "About Page")
+}
+
+func JSONHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "Welcome to Go Blog API",
+		"status":  "success",
+	})
 }
--- server.go
+++ server.go
@@ -9,6 +9,7 @@ func StartServer() {
 
 	http.HandleFunc("/", HomeHandler)
 	http.HandleFunc("/about", AboutHandler)
+	http.HandleFunc("/json", JSONHandler)
 	fmt.Println("Server Started: http://localhost:8080")
 
 	http.ListenAndServe(":8080", nil)
